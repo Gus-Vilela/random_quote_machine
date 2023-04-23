c800d6f6efe58a78840466a338c3e561fce13880
Gus-Vilela <gustavo.carvil@protonmail.com>
2023-04-22 21:35:46 -0300

    add quoteData, quote state

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,17 +1,37 @@
-import Buttons from './Buttons';
+import quoteData from "../quoteData";
+import {useState} from 'react';
 
 function Quote() {
+  const iniQuote = () => {
+    const quoteArray = quoteData.data.quotes;
+    const randomIndex = Math.floor(Math.random() * quoteArray.length);
+    const randomQuote = quoteArray[randomIndex];
+    return {quote : randomQuote.quote, author : randomQuote.author};
+  }
+  const [state, setState] = useState(iniQuote);
+  
+  const setQuote = () => {
+    const quoteArray = quoteData.data.quotes;
+    const randomIndex = Math.floor(Math.random() * quoteArray.length);
+    const randomQuote = quoteArray[randomIndex];
+    setState({quote : randomQuote.quote, author : randomQuote.author});
+  }
+
   return (
     <div id='quote-box'>
       <div>
         <p id='text'>
-          The best way to predict the future is to invent it.
+          {state.quote}
         </p>
         <p id='author'>
-          - Alan Kay
+          {state.author}
         </p>
       </div>
-      <Buttons />
+      <div id='buttons'>
+        <button id='new-quote' onClick={setQuote}>New Quote</button>
+        <a id='tweet-quote' href='twitter.com/intent/tweet' 
+        target='_blank'>Tweet Quote</a> 
+      </div>
     </div>
   )
 }
---
+++ src/quoteData.js
@@ -0,0 +1,31 @@
+export default {
+  "data" : {
+    'quotes' : [
+      {
+        'id' : 1,
+        'quote' : 'The best way to predict the future is to invent it.',
+        'author' : 'Alan Kay'
+      },
+      {
+        'id' : 2,
+        'quote' : 'The future is already here — it\'s just not very evenly distributed.',
+        'author' : 'William Gibson'
+      },
+      {
+        'id' : 3,
+        "quote": "I'm every bourgeois nightmare - a Cockney with intelligence and a million dollars.",
+        "author": "Michael Caine",
+      },
+      {
+        'id' : 4,
+        "quote": "To educate the intelligence is to expand the horizon of its wants and desires.",
+        "author": "James Russell Lowell",
+      },
+      {
+        'id' : 5,
+        "quote": "Science is the highest personification of the nation because that nation will remain the first which carries the furthest the works of thought and intelligence.",
+        "author": "Louis Pasteur",
+      }
+    ]
+  }
+}
