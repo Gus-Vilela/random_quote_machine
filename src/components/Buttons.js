49c1d643d5437e1798f98c7b4204b637c28db6cd
Gus-Vilela <gustavo.carvil@protonmail.com>
2023-05-03 19:48:32 -0300

    add fontawesome icons, icon tooltip

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,15 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+
 function Buttons (props) {
   const handleClick = () => {
     props.newQuote();
     props.changeColor();
   }
+
+  const icon = <FontAwesomeIcon icon={faTwitter} size='2x'/>
   
   return (
     <div id='buttons'>
+        
         <button id='new-quote' onClick={handleClick} style={{backgroundColor: props.color}}>New Quote</button>
+        <div className='tooltip'>
         <a id='tweet-quote' 
         href={props.url}
-        target='_blank'>Tweet Quote</a>
+        target='_blank' className='tooltip'>
+          {icon} 
+        </a>
+        <span className='tooltiptext'>Tweet this!</span>
+        </div>
     </div>
   )
 }
